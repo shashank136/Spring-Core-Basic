b5d825fd943e66eda6445de6cd3c646e2b6a04fc
Shashank <shashank136.sk@gmail.com>
2020-05-17 21:19:41 +0530

    constructor based injection

--- src/main/java/com/springprofile/profiles/controllers/GreetingService.java
+++ src/main/java/com/springprofile/profiles/controllers/GreetingService.java
@@ -1,5 +1,6 @@
 package com.springprofile.profiles.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.springprofile.profiles.services.HelloWorldService;
@@ -9,8 +10,8 @@ public class GreetingService {
 	
 	private HelloWorldService helloWorldService;
 	
-	
-	
+	// Constructor based injection
+	@Autowired
 	public GreetingService(HelloWorldService helloWorldService) {
 		this.helloWorldService = helloWorldService;
 	}
