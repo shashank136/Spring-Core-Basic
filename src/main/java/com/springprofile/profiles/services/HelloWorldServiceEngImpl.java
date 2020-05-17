af52c19242073fbae2ec9abbb67d3914c34620f1
Shashank <shashank136.sk@gmail.com>
2020-05-17 21:27:47 +0530

    Spring Profiles

--- src/main/java/com/springprofile/profiles/services/HelloWorldServiceEngImpl.java
+++ src/main/java/com/springprofile/profiles/services/HelloWorldServiceEngImpl.java
@@ -1,8 +1,10 @@
 package com.springprofile.profiles.services;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("english")
 public class HelloWorldServiceEngImpl implements HelloWorldService{
 
 	@Override
--- src/main/java/com/springprofile/profiles/services/HelloWorldServiceFrenchImpl.java
+++ src/main/java/com/springprofile/profiles/services/HelloWorldServiceFrenchImpl.java
@@ -1,6 +1,10 @@
 package com.springprofile.profiles.services;
 
-//@Component
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("french")
 public class HelloWorldServiceFrenchImpl implements HelloWorldService{
 
 	@Override
