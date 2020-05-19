8c887050c3fa8f9343ce65e616047cc1e52842cf
Shashank <shashank136.sk@gmail.com>
2020-05-19 21:24:40 +0530

    Implemented XML configuration

--- src/main/java/com/springprofile/SpringBoot/ProfilesApplication.java
+++ src/main/java/com/springprofile/SpringBoot/ProfilesApplication.java
@@ -3,12 +3,12 @@ package com.springprofile.SpringBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 import com.springprofile.profiles.controllers.GreetingService;
 
 @SpringBootApplication
-@ComponentScan("com.springprofile")
+@ImportResource("classpath:/spring/spring-config.xml")
 public class ProfilesApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/springprofile/profiles/controllers/GreetingService.java
+++ src/main/java/com/springprofile/profiles/controllers/GreetingService.java
@@ -2,12 +2,9 @@ package com.springprofile.profiles.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
 
 import com.springprofile.profiles.services.HelloWorldService;
-import com.springprofile.profiles.services.HelloWorldServiceSpanishImpl;
 
-@Controller
 public class GreetingService {
 	
 	private HelloWorldService helloWorldService;
@@ -16,19 +13,14 @@ public class GreetingService {
 	
 	private HelloWorldService helloWorldServiceHindi;
 	
-	@Autowired
 	public void setHelloWorldService(HelloWorldService helloWorldService) {
 		this.helloWorldService = helloWorldService;
 	}
 	
-	@Autowired
-	@Qualifier("helloWorldServiceSpanish") // Qualifiers takes the name of the method that implements the bean
 	public void setHelloWorldServiceSpanish(HelloWorldService helloWorldServiceSpanish) {
 		this.helloWorldServiceSpanish = helloWorldServiceSpanish;
 	}
 	
-	@Autowired
-	@Qualifier("hindi") // Qualifier also takes bean name which is more convenient then given the method name 
 	public void setHelloWorldServiceHindi(HelloWorldService helloWorldServiceHindi) {
 		this.helloWorldServiceHindi = helloWorldServiceHindi;
 	}
